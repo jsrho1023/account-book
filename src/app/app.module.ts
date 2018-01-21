8fae11b2fe568d5534d0bd554c568d3e372c2cfd
jason <technologytrip@gmail.com>
2018-01-21 22:24:55 +0900

    [comment] ngModel binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BudgetComponent } from "./budget/budget.component";
@@ -10,7 +11,8 @@ import { BudgetComponent } from "./budget/budget.component";
     BudgetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
