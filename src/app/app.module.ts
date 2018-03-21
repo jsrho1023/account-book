f4e28759fdae2180370dac6a12c6cb5c8a6409cc
jason <technologytrip@gmail.com>
2018-03-21 22:11:40 +0900

    [comment] mat-icon, mat-table added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -22,7 +24,9 @@ import { HeaderComponent } from './header/header.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 import { Observable } from 'rxjs/Rx';
 
 @Component({
@@ -7,13 +8,35 @@ import { Observable } from 'rxjs/Rx';
     styleUrls: [ './budget.component.css' ]
 })
 export class BudgetComponent implements OnInit{
-    budget: number = 1000;
+    budget: number = 20000;
+    displayedColumns = ['amount','desc'];
+    
+    consumptions : consumption[] = [{
+        amount: 12000,
+        desc: "food"
+    },{
+        amount: 10000,
+        desc: "beverage"
+    },{
+        amount: 12000,
+        desc: "dissert"
+    }];
+    dataSource = new MatTableDataSource(this.consumptions);
+    remain: number;
 
-    ngOnInit(){
-        
+    ngOnInit(){        
+        this.remain = this.budget;
+        this.consumptions.forEach(element => {
+            this.remain -= element.amount;
+        });            
     }
 
     onAddIncome(){
-        this.budget += this.budget;
+
     }
+}
+
+export interface consumption {
+    amount: number;
+    desc: string;
 }
