29e9338803f80bb264aa7bd6a6aeae46b644d6f4
jason <technologytrip@gmail.com>
2018-04-24 21:24:41 +0900

    [comment] use daily expense domain

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit} from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
-import { Consumption } from '../domain/consumption';
 import { FormControl } from '@angular/forms';
+import { DailyExpense } from '../domain/dailyExpense';
+import { Consumption } from '../domain/consumption';
 
 @Component({
     selector: 'app-budget',
@@ -13,20 +14,22 @@ export class BudgetComponent implements OnInit{
     displayedColumns = ['amount','desc'];
     date: FormControl;
     
-    consumptions = [
-        new Consumption(12000, "food"),
-        new Consumption(10000, "beverage"),
-        new Consumption(5000, "dissert")];
+    dailyExpense : DailyExpense;
 
-    dataSource = new MatTableDataSource(this.consumptions);
+    dataSource : MatTableDataSource<Consumption>; 
     remain: number;
 
-    ngOnInit(){        
-        this.remain = this.budget;
-        this.consumptions.forEach(element => {
+    ngOnInit(){
+        this.remain = this.budget;        
+        this.date = new FormControl(new Date());
+        this.dailyExpense = new DailyExpense(new Date());
+        this.dailyExpense.addConsumption(new Consumption(12000, "food"))
+        this.dailyExpense.addConsumption(new Consumption(10000, "beverage"))
+        this.dailyExpense.addConsumption(new Consumption(5000, "dissert"))
+        this.dataSource = new MatTableDataSource(this.dailyExpense.consumptions);
+        this.dailyExpense.consumptions.forEach(element => {
             this.remain -= element.amount;
         });
-        this.date = new FormControl(new Date());
     }
 
     onAdd(){
