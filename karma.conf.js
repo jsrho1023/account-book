9b91d6bf00d05b984e14277ca3ce6ca5cd4c6d87
jason <technologytrip@gmail.com>
2018-10-31 21:45:50 +0900

    [comment] set karma to use phantomJS launcher

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -27,7 +27,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: true
+    browsers: ['PhantomJS'],
+    singleRun: true,
+
+    phantomjsLauncher: {
+      exitOnResourceError: true
+    }
   });
 };
