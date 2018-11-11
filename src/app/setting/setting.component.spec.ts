8e6346e780b0fb5442f66dc49b165822e19c9b81
jason <technologytrip@gmail.com>
2018-11-11 23:42:11 +0900

    [comment] setting component ui added, input field shape changed

--- src/app/setting/setting.component.spec.ts
+++ src/app/setting/setting.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingComponent } from './setting.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule, MatButtonModule } from '@angular/material';
+import { NgxsModule } from '@ngxs/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SettingComponent', () => {
   let component: SettingComponent;
@@ -8,7 +12,14 @@ describe('SettingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SettingComponent ]
+      declarations: [ SettingComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatButtonModule,
+        NgxsModule.forRoot([])]
     })
     .compileComponents();
   }));
@@ -22,4 +33,20 @@ describe('SettingComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Render', () => {
+    describe('when loaded', () => {
+      it('then have three input fields', () => {
+        const settingComponent: HTMLElement = fixture.nativeElement;
+        const formElements = settingComponent.querySelectorAll("mat-form-field");
+        expect(formElements.length).toEqual(3)
+      })
+
+      it('then have a save button disabled', () => {
+        const settingComponent: HTMLElement = fixture.nativeElement;
+        const buttonElement = settingComponent.querySelector(".setting button:disabled");        
+        expect(buttonElement).toBeTruthy()
+      })
+    })
+  })
 });
--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import { Store } from '@ngxs/store';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class SettingErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+}
 
 @Component({
   selector: 'app-setting',
@@ -6,9 +16,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setting.component.css']
 })
 export class SettingComponent implements OnInit {
+  settingForm = new FormGroup({
+    income: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\d+$/)
+    ]),
+    expense: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\d+$/)
+    ]),
+    saving: new FormControl(10000, [
+      Validators.required,
+      Validators.pattern(/^\d+$/)
+    ])
+  });
 
-  constructor() { }
+  dailyBudget: number = 10000;
+  matcher = new SettingErrorStateMatcher();
+
+  constructor(public Store: Store) { }
 
   ngOnInit() {
+  }
+
+  onSave() {
+    this.settingForm.reset();
   }
 }
